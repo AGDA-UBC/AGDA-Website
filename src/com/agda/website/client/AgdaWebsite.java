a1e8e049cb266ea887f9cd9dfd0f7db553ecee15
raptorone <philip.stachura@unoworks.ca>
2019-08-22 16:24:53 -0700

    Initial commit

---
+++ src/com/agda/website/client/AgdaWebsite.java
@@ -0,0 +1,18 @@
+package com.agda.website.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import java.util.logging.Logger;
+
+public class AgdaWebsite implements EntryPoint
+{
+
+    Logger logger = Logger.getLogger("AgdaWebsite");
+
+    public void onModuleLoad()
+    {
+        logger.info( "Test" );
+    }
+
+
+}
